7cbfae2d20553107336b638f74ab59bd8eaa403c
sebastian nieto <juan.nieto-mo@mail.escuelaing.edu.co>
2021-03-08 22:14:31 -0500

    intento 2 con heroku

--- src/main/java/edu/escuelaing/edu/co/app/AppApplication.java
+++ src/main/java/edu/escuelaing/edu/co/app/AppApplication.java
@@ -8,9 +8,7 @@ import java.util.Collections;
 public class AppApplication {
 
 	public static void main(String[] args) {
-		SpringApplication app = new SpringApplication(AppApplication.class);
-		app.setDefaultProperties(Collections.singletonMap("server.port", "8083"));
-		app.run(args);
+		SpringApplication.run(AppApplication.class, args);
 	}
 
 }
